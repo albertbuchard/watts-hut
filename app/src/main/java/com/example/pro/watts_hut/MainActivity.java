e5ac816d1fc60c8523eee849dc90055f8e58eda2
albert.buchard <antinabi87>
2017-02-16 19:43:01 +0100

    fixes #11 - fixes #17; This is the third time.

--- app/src/main/java/com/example/pro/watts_hut/MainActivity.java
+++ app/src/main/java/com/example/pro/watts_hut/MainActivity.java
@@ -102,7 +102,6 @@ public class MainActivity extends AppCompatActivity {
             Log.d(TAG, "loadMovies: "+url);
 
             new loadDataTask().execute(url);
-
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
