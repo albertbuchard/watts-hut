bb358929818786752817c5c3d210c403ce014a90
albert.buchard <antinabi87>
2017-02-16 22:23:44 +0100

    fixes #18; Images are loaded into the gridview as the user scrolls.

--- app/src/main/java/com/example/pro/watts_hut/MainActivity.java
+++ app/src/main/java/com/example/pro/watts_hut/MainActivity.java
@@ -1,34 +1,36 @@
 package com.example.pro.watts_hut;
 
 import android.content.Context;
-import android.net.NetworkRequest;
 import android.net.Uri;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.GridView;
-import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.squareup.picasso.Picasso;
+import org.apache.commons.lang3.ArrayUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
-import java.net.URI;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private int currentPage;
     private Toast internalToast;
+    private MovieAdapter internalMovieAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,15 +38,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Picasso.with(getApplicationContext()).load("http://i.imgur.com/DvpvklR.png").into((ImageView) findViewById(R.id.testView));
-
+        internalMovieAdapter = new MovieAdapter(this);
         GridView gridview = (GridView) findViewById(R.id.movieGridView);
-        gridview.setAdapter(new movieAdapter(this));
+        gridview.setAdapter(internalMovieAdapter);
 
         gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v,
                                     int position, long id) {
-                Toast.makeText(getApplicationContext(), "" + position,
-                        Toast.LENGTH_SHORT).show();
+                boast(Toast.makeText(getApplicationContext(), "" + position, Toast.LENGTH_SHORT));
+            }
+        });
+
+        gridview.setOnScrollListener(new AbsListView.OnScrollListener(){
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount)
+            {
+                if(firstVisibleItem + visibleItemCount >= totalItemCount){
+                    loadMovies();
+                }
+            }
+
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState){
+
             }
         });
     }
@@ -68,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int menuItemId = item.getItemId();
         if (menuItemId == R.id.action_refresh) {
-            Context context = MainActivity.this;
+            Context context = this;
             String message = "Refresh!";
             boast(Toast.makeText(context, message, Toast.LENGTH_LONG));
             loadMovies();
@@ -92,7 +108,7 @@ public class MainActivity extends AppCompatActivity {
         URL url = null;
         try {
             url = new URL(builtURI.toString());
-            Context context = MainActivity.this;
+            Context context = getApplicationContext();
             String message = "URL:"+url;
 
 
@@ -108,10 +124,10 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    class loadDataTask extends AsyncTask<URL, Void, String> {
+    class loadDataTask extends AsyncTask<URL, Void, JSONObject> {
 
         @Override
-        protected String doInBackground(URL... urls) {
+        protected JSONObject doInBackground(URL... urls) {
             URL url = urls[0];
             try {
                 HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
@@ -127,9 +143,11 @@ public class MainActivity extends AppCompatActivity {
                         stringBuilder.append(line).append("\n");
                     }
                     bufferedReader.close();
-                    return stringBuilder.toString();
-                }
-                finally{
+                    JSONObject data = new JSONObject(stringBuilder.toString());
+                    return data;
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                } finally{
                     urlConnection.disconnect();
                 }
             } catch (IOException e) {
@@ -140,13 +158,34 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        protected void onPostExecute(String response) {
+        protected void onPostExecute(JSONObject response) {
             if(response == null) {
-                response = "THERE WAS AN ERROR";
+                Log.i("INFO", "THERE WAS AN ERROR");
             }
 
-            Log.i("INFO", response);
-            boast(Toast.makeText(getApplicationContext(), response, Toast.LENGTH_SHORT));
+            try {
+                JSONArray results = response.getJSONArray("results");
+                currentPage = response.getInt("page")+1;
+
+                String[] imageUrls = new String[results.length()];
+                for(int n = 0; n < results.length(); n++)
+                {
+                    JSONObject movie = results.getJSONObject(n);
+                    imageUrls[n] = movie.getString(getString(R.string.movie_api_results_image));
+                    if (imageUrls[n] == "null")
+                        continue;
+                    imageUrls[n] = String.format(getString(R.string.image_api_url), "w185", imageUrls[n]);
+                    Log.i(TAG, "onPostExecute: "+imageUrls[n]);
+                }
+                imageUrls = ArrayUtils.removeAllOccurences(imageUrls, "null");
+                internalMovieAdapter.addUrls(imageUrls);
+                internalMovieAdapter.notifyDataSetChanged();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
+//            boast(Toast.makeText(getApplicationContext(), response, Toast.LENGTH_SHORT));
         }
 
     }
