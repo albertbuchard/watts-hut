31ff44d24d2414b855a0fde5b231b724b298db57
albert.buchard <antinabi87>
2017-02-20 02:45:10 +0100

    fixes #2 - Fixes # 9 - Fixes #21 - Fixes #8 - Fixes #13 - Fixes #19 - Fixes #12; Converted gridview to recyclerview, fiddled a bit with the toolbar, fixed the click, and allowed for smoother scrolling by caching the images as soon as their url is known.

--- app/src/main/java/com/example/pro/watts_hut/MainActivity.java
+++ app/src/main/java/com/example/pro/watts_hut/MainActivity.java
@@ -1,18 +1,21 @@
 package com.example.pro.watts_hut;
 
+import android.app.ActionBar;
 import android.content.Context;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AbsListView;
-import android.widget.AdapterView;
-import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
 
 import org.apache.commons.lang3.ArrayUtils;
 import org.json.JSONArray;
@@ -25,44 +28,48 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashSet;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private int currentPage;
     private Toast internalToast;
-    private MovieAdapter internalMovieAdapter;
+    private MovieRvAdapter internalMovieAdapter;
+    private RecyclerView recyclerView;
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Picasso.with(getApplicationContext()).load("http://i.imgur.com/DvpvklR.png").into((ImageView) findViewById(R.id.testView));
-        internalMovieAdapter = new MovieAdapter(this);
-        GridView gridview = (GridView) findViewById(R.id.movieGridView);
-        gridview.setAdapter(internalMovieAdapter);
+        mContext = getApplicationContext();
 
-        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View v,
-                                    int position, long id) {
-                boast(Toast.makeText(getApplicationContext(), "" + position, Toast.LENGTH_SHORT));
+        // Hide UI - Go Fullscreen
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        ActionBar actionBar = getActionBar();
+        if (actionBar != null)
+            actionBar.hide();
+
+        // Setup recycler view
+        recyclerView = (RecyclerView) findViewById(R.id.rv_movie_grid) ;
+
+        // Set GridLayoutManager to the recyclerview, with two columns
+        GridLayoutManager layoutManager = new GridLayoutManager(this,2);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // Set the adapter to the recycler view
+        internalMovieAdapter = new MovieRvAdapter(this, recyclerView);
+        internalMovieAdapter.setOnLoadMoreListener(new MovieRvAdapter.OnLoadMoreListener() {
+            @Override
+            public void onLoadMore() {
+                loadMovies();
             }
         });
+        recyclerView.setAdapter(internalMovieAdapter);
 
-        gridview.setOnScrollListener(new AbsListView.OnScrollListener(){
-            @Override
-            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount)
-            {
-                if(firstVisibleItem + visibleItemCount >= totalItemCount){
-                    loadMovies();
-                }
-            }
-
-            @Override
-            public void onScrollStateChanged(AbsListView view, int scrollState){
-
-            }
-        });
     }
 
     @Override
@@ -70,6 +77,19 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+
+//    private void hideViews() {
+//        mToolbar.animate().translationY(-mToolbar.getHeight()).setInterpolator(new AccelerateInterpolator(2));
+//
+//        FrameLayout.LayoutParams lp = (FrameLayout.LayoutParams) mFabButton.getLayoutParams();
+//        int fabBottomMargin = lp.bottomMargin;
+//        mFabButton.animate().translationY(mFabButton.getHeight()+fabBottomMargin).setInterpolator(new AccelerateInterpolator(2)).start();
+//    }
+//
+//    private void showViews() {
+//        mToolbar.animate().translationY(0).setInterpolator(new DecelerateInterpolator(2));
+//        mFabButton.animate().translationY(0).setInterpolator(new DecelerateInterpolator(2)).start();
+//    }
 
     public void boast(Toast toast) {
         if (internalToast != null) {
@@ -99,7 +119,7 @@ public class MainActivity extends AppCompatActivity {
 
         currentPage += 1;
 
-        Uri builtURI = Uri.parse(getString(R.string.movie_api_url)).buildUpon()
+        Uri builtURI = Uri.parse(getString(R.string.movie_api_popular)).buildUpon()
                 .appendQueryParameter(getString(R.string.movie_api_param_key), apiKey)
                 .appendQueryParameter(getString(R.string.movie_api_param_sort), sortingArray[0])
                 .appendQueryParameter(getString(R.string.movie_api_param_page), Integer.toString(currentPage))
@@ -124,17 +144,23 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    class loadDataTask extends AsyncTask<URL, Void, JSONObject> {
+    class loadDataTask extends AsyncTask<URL, Void, HashSet<MovieObject>> {
 
         @Override
-        protected JSONObject doInBackground(URL... urls) {
+        protected HashSet<MovieObject> doInBackground(URL... urls) {
             URL url = urls[0];
+            String[] imageUrls = new String[0];
+            HashSet<MovieObject> movieList = new HashSet<MovieObject>();
+
             try {
+
                 HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
                 urlConnection.setRequestMethod("GET");
                 urlConnection.setDoOutput(true);
                 urlConnection.setDoInput(true);
+                urlConnection.setUseCaches(true);
                 urlConnection.connect();
+
                 try {
                     BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
                     StringBuilder stringBuilder = new StringBuilder();
@@ -143,13 +169,52 @@ public class MainActivity extends AppCompatActivity {
                         stringBuilder.append(line).append("\n");
                     }
                     bufferedReader.close();
+
                     JSONObject data = new JSONObject(stringBuilder.toString());
-                    return data;
+
+                    try {
+                        JSONArray results = data.getJSONArray("results");
+                        currentPage = data.getInt("page")+1;
+
+                        imageUrls = new String[results.length()];
+                        for(int n = 0; n < results.length(); n++)
+                        {
+                            JSONObject movie = results.getJSONObject(n);
+
+
+                            imageUrls[n] = movie.getString(getString(R.string.movie_api_results_image));
+
+                            if (imageUrls[n] == "null")
+                                continue;
+
+                            MovieObject movieObject = new MovieObject(movie);
+                            movieList.add(movieObject);
+
+                            imageUrls[n] = String.format(getString(R.string.image_api_url), "w185", imageUrls[n]);
+
+                            try {
+                                Picasso.with(mContext)
+                                        .load(imageUrls[n])
+                                        .get();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+
+                            Log.i(TAG, "doInBackground: "+imageUrls[n]);
+                        }
+
+                        imageUrls = ArrayUtils.removeAllOccurences(imageUrls, "null");
+                        return movieList;
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
                 } catch (JSONException e) {
                     e.printStackTrace();
                 } finally{
                     urlConnection.disconnect();
                 }
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -158,36 +223,42 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        protected void onPostExecute(JSONObject response) {
-            if(response == null) {
+        protected void onPostExecute(HashSet<MovieObject> movieList) {
+            if((movieList == null)||(movieList.size() == 0)) {
                 Log.i("INFO", "THERE WAS AN ERROR");
+            } else {
+                internalMovieAdapter.addMovies(movieList);
             }
-
-            try {
-                JSONArray results = response.getJSONArray("results");
-                currentPage = response.getInt("page")+1;
-
-                String[] imageUrls = new String[results.length()];
-                for(int n = 0; n < results.length(); n++)
-                {
-                    JSONObject movie = results.getJSONObject(n);
-                    imageUrls[n] = movie.getString(getString(R.string.movie_api_results_image));
-                    if (imageUrls[n] == "null")
-                        continue;
-                    imageUrls[n] = String.format(getString(R.string.image_api_url), "w185", imageUrls[n]);
-                    Log.i(TAG, "onPostExecute: "+imageUrls[n]);
-                }
-                imageUrls = ArrayUtils.removeAllOccurences(imageUrls, "null");
-                internalMovieAdapter.addUrls(imageUrls);
-                internalMovieAdapter.notifyDataSetChanged();
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-
-
 //            boast(Toast.makeText(getApplicationContext(), response, Toast.LENGTH_SHORT));
         }
 
     }
+
+    public class MovieObject {
+        public JSONObject data;
+        public ImageView cachedImage;
+        public String imageUrl;
+
+        public MovieObject(JSONObject jsonObject) {
+            data = jsonObject;
+
+            try {
+                imageUrl = jsonObject.getString(getString(R.string.movie_api_results_image));
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        public String getThumbnailUrl() {
+            String url = "null";
+
+            if (imageUrl != "null")
+                url = String.format(getString(R.string.image_api_url), "w185", imageUrl);
+
+            return url;
+        }
+    }
+
 
 }
---
+++ app/src/main/java/com/example/pro/watts_hut/MovieRvAdapter.java
@@ -0,0 +1,203 @@
+package com.example.pro.watts_hut;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.widget.ContentLoadingProgressBar;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridLayout;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.example.pro.watts_hut.MainActivity.MovieObject;
+import com.squareup.picasso.Picasso;
+
+import org.apache.commons.lang3.ArrayUtils;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+
+/**
+ * Created by Pro on 19.02.17.
+ */
+
+public class MovieRvAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private static final String TAG = MovieRvAdapter.class.getSimpleName();
+    public Context appContext;
+    private final int VIEW_ITEM = 1;
+    private final int VIEW_PROG = 0;
+    private GridLayoutManager gridLayoutManager;
+
+    public List<MovieObject> mDataset = new ArrayList<MovieObject>();
+    private OnLoadMoreListener onLoadMoreListener;
+
+    private int visibleThreshold = 20;
+    private int lastVisibleItem, totalItemCount;
+    private boolean loading;
+
+    public MovieRvAdapter (Context c, RecyclerView recyclerView) {
+        appContext = c;
+        if (recyclerView.getLayoutManager() instanceof GridLayoutManager) {
+
+            gridLayoutManager = (GridLayoutManager) recyclerView.getLayoutManager();
+
+
+            recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+                @Override
+                public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                    super.onScrolled(recyclerView, dx, dy);
+
+                    checkVisibleAndLoad();
+                }
+            });
+        } else {
+            Log.e(TAG, "MovieRvAdapter: Wrong layout manager", new Exception());
+        }
+    }
+
+    public void checkVisibleAndLoad() {
+        totalItemCount = gridLayoutManager.getItemCount();
+        lastVisibleItem = gridLayoutManager.findLastVisibleItemPosition();
+        if (!loading && totalItemCount <= (lastVisibleItem + visibleThreshold)) {
+
+            if (onLoadMoreListener != null) {
+                onLoadMoreListener.onLoadMore();
+            }
+
+            loading = true;
+        }
+    }
+
+    public void addMovies (Collection<MovieObject> movieObjectList) {
+        mDataset.addAll(movieObjectList);
+        this.notifyDataSetChanged();
+        loading = false;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        Context context = viewGroup.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        int layoutIdForListItem = R.layout.movie_list_item;
+        int layoutIdForProgressItem = R.layout.support_simple_spinner_dropdown_item;
+        boolean shouldAttachToParentImmediately = false;
+
+        RecyclerView.ViewHolder viewHolder;
+
+        if (viewType == VIEW_ITEM) {
+            View view = inflater.inflate(layoutIdForListItem, viewGroup, shouldAttachToParentImmediately);
+            viewHolder = new MovieViewHolder(view);
+        } else {
+            View view = inflater.inflate(layoutIdForProgressItem, viewGroup, shouldAttachToParentImmediately);
+            viewHolder = new ProgressViewHolder(view);
+        }
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+        Log.d(TAG, "#" + position);
+        //GridLayoutManager.LayoutParams layoutParams = (GridLayoutManager.LayoutParams) holder.itemView.getLayoutParams();
+
+        if (holder instanceof MovieViewHolder) {
+            MovieViewHolder cast = (MovieViewHolder) holder;
+
+            cast.bind(position);
+        } else if (holder instanceof ProgressViewHolder) {
+            ProgressViewHolder cast = (ProgressViewHolder) holder;
+            cast.bind();
+        }
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return (mDataset.size());
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return mDataset.get(position) != null ? VIEW_ITEM : VIEW_PROG;
+    }
+
+    public void setOnLoadMoreListener(OnLoadMoreListener onLoadMoreListener) {
+        this.onLoadMoreListener = onLoadMoreListener;
+        onLoadMoreListener.onLoadMore();
+        this.checkVisibleAndLoad();
+    }
+
+    public interface OnLoadMoreListener {
+        void onLoadMore();
+    }
+
+    class MovieViewHolder extends RecyclerView.ViewHolder
+        implements View.OnClickListener {
+        ImageView movieThumbnail;
+        private int position = -1;
+
+        public MovieViewHolder (View itemView) {
+            super(itemView);
+            itemView.setOnClickListener(this);
+            movieThumbnail = (ImageView) itemView.findViewById(R.id.rv_movie_thumbnail);
+        }
+
+        void bind (int position) {
+            this.position = position;
+            // Get display size
+            DisplayMetrics display = ((Activity) appContext).getResources().getDisplayMetrics();
+            int width = display.widthPixels;
+            int imageWidth = width / 2;
+            int imageHeight = (int) Math.round(display.heightPixels/2.5);
+
+            // Set layout parameter of the thumbnail to make sure it is fullbleed
+            Log.i(TAG, "bind: parent is" + movieThumbnail.getParent().toString());
+
+            //movieThumbnail.setLayoutParams();
+            movieThumbnail.setAdjustViewBounds(true);
+            movieThumbnail.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            movieThumbnail.setPadding(0, 0, 0, 0);
+
+            MovieObject movie = mDataset.get(position);
+            if (movie != null) {
+                Picasso.with(appContext)
+                        .load(movie.getThumbnailUrl())
+                        .resize(imageWidth,imageHeight)
+                        .centerCrop()
+                        .into(movieThumbnail);
+            }
+        }
+
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(appContext, Integer.toString(this.position), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    class ProgressViewHolder extends RecyclerView.ViewHolder {
+        private ContentLoadingProgressBar spinner;
+
+        public ProgressViewHolder(View itemView) {
+            super(itemView);
+
+            spinner = (ContentLoadingProgressBar) itemView.findViewById(R.id.progressBar);
+        }
+
+        void bind () {
+            spinner.show();
+        }
+    }
+}
